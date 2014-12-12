1321450cf3bda350e35b3034a54d2cf107f3d441
SefaCaksu <sefacaksu@gmail.com>
2014-12-12 18:30:02 +0200

    ilk pro

--- UniverseFramework/Class1.cs
+++ UniverseFramework/Class1.cs
@@ -8,5 +8,13 @@ namespace UniverseFramework
 {
     public class Class1
     {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        
     }
 }
