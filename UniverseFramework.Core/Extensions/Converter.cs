c18a0cf0cde0be0a812c1caf02785c172228771e
ulkardan <ulkardan@gmail.com>
2014-12-14 23:16:56 +0200

    Ara Commit

--- UniverseFramework.Core/Extensions/Converter.cs
+++ UniverseFramework.Core/Extensions/Converter.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace UniverseFramework.Core.Extensions
+namespace UniverseFramework.Core.Extensions.Converters
 {
     [DebuggerStepThrough]
     public static class Converter
---
+++ UniverseFramework.Core/Extensions/Global.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniverseFramework.Core.Extensions.Converters;
+
+namespace UniverseFramework.Core
+{
+    public static class Global
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+        public static bool IsNotNull(this object obj)
+        {
+            return obj != null;
+        }
+
+        public static Nullable<T> ConvertTo<T>(this object value) where T : struct, IComparable
+        {
+            if (value.IsNull())
+            {
+                return null;
+            }
+
+            if (!typeof(T).IsValueType)
+            {
+                return null;
+            }
+
+            if(typeof(T).Equals(typeof(bool)))
+            {
+                return (T?)value.ToBooleanVal();
+            }
+
+
+
+            return null;
+        }
+
+        public static T GetPropVal<T>(this object obj,string propertyName)
+        {
+
+        }
+    }
+}
